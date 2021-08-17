a6d65a3ed5e25456894e528d087340230b08a56d
effendikiki <43091431+effendikiki@users.noreply.github.com>
2021-08-17 13:01:27 +0700

    static body done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   purge: ["./src/**/*.{html,js}"],
   darkMode: false, // or "media" or "class"
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: "#ffb938",
+          dark: "#241700",
+          DEFAULT: "#fba300"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
